02efdb23b52b39e3e91a8a670831e9885dd063ef
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-13 18:59:28 +0530

    Business card

--- BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Phone
 import androidx.compose.material3.Icon
@@ -23,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -45,8 +47,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ShowIntro(image: Int, name: String, title: String, modifier: Modifier= Modifier) {
+fun DrawTheCard() {
     Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(8.dp)) {
+        ShowIntro(
+            modifier = Modifier.weight(3f),
+            image = R.drawable.me,
+            name = "Mohammad Zaid Aziz",
+            title = "Student"
+        )
+        ContactDetails(Modifier.weight(2f))
+    }
+}
+
+@Composable
+fun ShowIntro(image: Int, name: String, title: String, modifier: Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
         Image(
             painter = painterResource(id = image),
             contentDescription = null,
@@ -66,50 +87,26 @@ fun ShowIntro(image: Int, name: String, title: String, modifier: Modifier= Modif
 }
 
 @Composable
-fun ContactDetails() {
-    Column(horizontalAlignment = Alignment.Start, modifier = Modifier.padding(8.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(4.dp)) {
-            Icon(Icons.Rounded.Phone, contentDescription = null, modifier = Modifier.size(32.dp))
-            Text(text = "02020201010", modifier = Modifier.padding(start = 8.dp))
-        }
-        Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(4.dp)){
-            //TODO:  add alternate email emoji
-            Icon(Icons.Outlined.Email, contentDescription = null, modifier = Modifier.size(32.dp))
-            Text(text = "@how are you i am fine", modifier = Modifier.padding(start = 8.dp))
-        }
-        Row (verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(4.dp)){
-            Icon(Icons.Rounded.Email, contentDescription = null, modifier = Modifier.size(32.dp))
-            Text(text = "02020201010", modifier = Modifier.padding(start = 8.dp))
-        }
+fun ContactDetails(modifier: Modifier) {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        ContactRow(text = "+91 1234567890", icon = Icons.Rounded.Phone, modifier = Modifier.padding(4.dp))
+        ContactRow(text = "@mohammadzaidaziz", icon = Icons.Rounded.Email, modifier = Modifier.padding(4.dp))
+        ContactRow(text = "conect@connect", icon =Icons.Default.Email, modifier =Modifier.padding())
 
     }
 }
 
 @Composable
-fun DrawTheCard() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(8.dp)) {
-        ShowIntro(image = R.drawable.me, name = "Mohammad Zaid Aziz", title = "Student")
-        ContactDetails()
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun ContactDetailsPreview() {
-    BusinessCardAppTheme {
-        ContactDetails()
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    BusinessCardAppTheme {
-        ShowIntro(
-            image = R.drawable.me,
-            name = "Mohammad Zaid Aziz",
-            title = "Student"
-        )
+fun ContactRow(text: String, icon: ImageVector, modifier: Modifier) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(4.dp)) {
+        Icon(icon, contentDescription = null, modifier = Modifier.size(32.dp))
+        Text(text = text, modifier = Modifier.padding(start = 8.dp))
     }
 }
 
