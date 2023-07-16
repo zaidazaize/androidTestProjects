3572e59d9cceded3deb062a37c1a3fc6f13589b7
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-16 22:46:20 +0530

    Step 8 OnBoardingScreen

--- BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.basiccodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,36 +30,96 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicCodeLabTheme {
                 // A surface container using the 'background' color from the theme
-               MyApp(modifier = Modifier.fillMaxSize())
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier){
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-       Greeting(name = "Android","how are you?")
+fun OnBoardingScreen(onContinueClicked: () -> Unit) {
+    var shouldShowOnBoarding by remember {
+        mutableStateOf(true)
     }
-}
-
-@Composable
-fun Greeting(name: String,message : String) {
-    Surface(color=MaterialTheme.colorScheme.primary) {
-        Column(modifier = Modifier.padding(24.dp)) {
-            Text(text = "Hello $name!")
-            Text(text = message, )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the app")
+        Button(
+            onClick = onContinueClicked,
+            modifier = Modifier.padding(vertical = 24.dp)
+        ) {
+            Text(text = "Continue")
         }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnBoarding by remember {
+        mutableStateOf(true)
+    }
+    if(shouldShowOnBoarding) {
+        OnBoardingScreen({ shouldShowOnBoarding = false})
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("onea;l", "two", "three")
+) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(modifier = modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(name = name, "how are you?")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, message: String) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+    ) {
+        var expanded by remember {
+            mutableStateOf(false)
+        }
+        val padding = if (expanded) 48.dp else 0.dp
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello $name!")
+                Text(text = message, modifier = Modifier.padding(bottom = padding))
+            }
+
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show Less" else "Show More")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     BasicCodeLabTheme {
-        Greeting("Android","how are you?")
+        MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun OnBoardingScreenPreview() {
+    BasicCodeLabTheme {
+        OnBoardingScreen(onContinueClicked = {})
     }
 }
