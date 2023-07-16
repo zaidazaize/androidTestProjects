37154151432e5fe797c1a492d6fe73812e961ecc
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-16 18:29:57 +0530

    Step 5 done

--- BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.basiccodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basiccodelab.ui.theme.BasicCodeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,20 +22,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicCodeLabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
+               MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
+@Composable
+fun MyApp(modifier: Modifier = Modifier){
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+       Greeting(name = "Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color=MaterialTheme.colorScheme.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicCodeLabTheme {
+        Greeting("Android")
     }
 }
