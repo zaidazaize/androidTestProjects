631a26f96cc7f90032ac8b288bde4dde87aaf436
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-17 11:21:19 +0530

    complete Codelab

--- BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ BasicCodeLab/app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.basiccodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,6 +34,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelab.ui.theme.BasicCodeLabTheme
@@ -44,7 +53,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnBoarding by rememberSaveable() {
+    var shouldShowOnBoarding by rememberSaveable {
         mutableStateOf(true)
     }
     if (shouldShowOnBoarding) {
@@ -56,7 +65,8 @@ fun MyApp(modifier: Modifier = Modifier) {
 
 @Composable
 fun OnBoardingScreen(onContinueClicked: () -> Unit) {
-    var shouldShowOnBoarding by remember {
+    //rememberSavable is used to save the state of the variable is preserved in the event of a configuration changes
+    var shouldShowOnBoarding by rememberSaveable {
         mutableStateOf(true)
     }
     Column(
@@ -66,8 +76,7 @@ fun OnBoardingScreen(onContinueClicked: () -> Unit) {
     ) {
         Text(text = "Welcome to the app")
         Button(
-            onClick = onContinueClicked,
-            modifier = Modifier.padding(vertical = 24.dp)
+            onClick = onContinueClicked, modifier = Modifier.padding(vertical = 24.dp)
         ) {
             Text(text = "Continue")
         }
@@ -76,12 +85,10 @@ fun OnBoardingScreen(onContinueClicked: () -> Unit) {
 
 @Composable
 fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { index -> "Android $index" }
+    modifier: Modifier = Modifier, names: List<String> = List(1000) { index -> "Android $index" }
 ) {
     Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
+        modifier = modifier, color = MaterialTheme.colorScheme.background
     ) {
         LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
             items(items = names) { name ->
@@ -93,30 +100,64 @@ fun Greetings(
 
 @Composable
 fun Greeting(name: String, message: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+        ), modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name = name, message = message)
+    }
+}
+
+@Composable
+fun CardContent(name: String, message: String) {
     var expanded by remember {
         mutableStateOf(false)
     }
-    val padding by animateDpAsState(
-        targetValue = if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f).padding(bottom = padding.coerceAtLeast(0.dp))) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .weight(1f)
+
+            ) {
                 Text(text = "Hello $name!")
-                Text(text = message)
+                Text(
+                    text = message.repeat(7),
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold),
+                    maxLines = if (expanded) Int.MAX_VALUE else 1
+                )
             }
 
-            ElevatedButton(onClick = { expanded = !expanded }) {
-                Text(text = if (expanded) "Show Less" else "Show More")
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(modifier = Modifier.size(24.dp),
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) "Expand less" else "Expand more"
+                )
             }
         }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    BasicCodeLabTheme {
+        Greeting(name = "Android", message = "Welcome to the app")
     }
 }
 
@@ -133,5 +174,23 @@ fun GreetingPreview() {
 fun OnBoardingScreenPreview() {
     BasicCodeLabTheme {
         OnBoardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview(
+    name = "Dark", showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun DarkPreview() {
+    BasicCodeLabTheme {
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, )
+@Composable
+fun CardContentPreview() {
+    BasicCodeLabTheme {
+        Text(text = "Hello World!", modifier = Modifier.padding(12.dp))
     }
 }
--- BasicCodeLab/app/src/main/java/com/example/basiccodelab/ui/theme/Theme.kt
+++ BasicCodeLab/app/src/main/java/com/example/basiccodelab/ui/theme/Theme.kt
@@ -10,21 +10,24 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+   surface = Blue,
+   onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -45,25 +48,26 @@ fun BasicCodeLabTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
-      SideEffect {
-        val window = (view.context as Activity).window
-        window.statusBarColor = colorScheme.primary.toArgb()
-        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-      }
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
