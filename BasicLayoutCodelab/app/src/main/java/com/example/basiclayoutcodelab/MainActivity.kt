85fca7376f18e96f21982e8644707eed9afb7abe
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-18 21:42:53 +0530

    Building blocks step 7

--- BasicLayoutCodelab/app/src/main/java/com/example/basiclayoutcodelab/MainActivity.kt
+++ BasicLayoutCodelab/app/src/main/java/com/example/basiclayoutcodelab/MainActivity.kt
@@ -3,13 +3,35 @@ package com.example.basiclayoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basiclayoutcodelab.ui.theme.BasicLayoutCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +50,108 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(),
+        placeholder = { Text(text = "Search") },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+            .background(MaterialTheme.colorScheme.surface)
+    )
+}
 
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier,
+    image: Int,
+    text: String
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(88.dp)
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun FavouriteCollectionElement(modifier: Modifier = Modifier, image: Int, text: String) {
+    Surface( shape = MaterialTheme.shapes.small, modifier = modifier,
+    ) {
+        Row(
+            modifier = Modifier.width(192.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(56.dp)
+            )
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FavouriteCollectionElementPreview() {
+    BasicLayoutCodelabTheme {
+        FavouriteCollectionElement(
+            modifier = Modifier.padding(8.dp),
+            image = R.drawable.ab1_inversions,
+            text = "Inversions"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlignYourBodyElementPreview() {
+    BasicLayoutCodelabTheme {
+        AlignYourBodyElement(
+            modifier = Modifier.padding(8.dp),
+            image = R.drawable.ab1_inversions,
+            text = "Inversions"
+        )
+    }
+}
+
+@Preview()
+@Composable
+fun SearchBarPreview() {
+    BasicLayoutCodelabTheme {
+        SearchBar()
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
