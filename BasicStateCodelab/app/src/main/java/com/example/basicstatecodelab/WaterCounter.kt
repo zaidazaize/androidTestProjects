568442ff783dfef30b8f8db6fe65cc1de2ce4262
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-27 18:16:30 +0530

    8

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,7 +1,6 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -9,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,34 +16,21 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember {
+        var count by rememberSaveable {
             mutableStateOf(0)
         }
         if (count > 0) {
-            var showTask by remember {
-                mutableStateOf(true)
-            }
-            if (showTask) {
-                WellnessTaskItem(taskName = "Have you taken 15 minutes to walk today?",
-                    onClose = { showTask = false })
-            }
             Text(
                 text = "You have ${count} glasses",
             )
         }
-        Row(modifier = Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = { count++ }, enabled = count < 10
-            ) {
-                Text(text = "Add One")
-            }
-            Button(
-                onClick = { count = 0 },
-                modifier = Modifier.padding(start = 16.dp),
-            ) {
-                Text(text = "Reset")
-            }
+
+        Button(
+            onClick = { count++ }, enabled = count < 10
+        ) {
+            Text(text = "Add One")
         }
+
     }
 
 }
