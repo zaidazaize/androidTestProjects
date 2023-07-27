f838f3c74431f169de7b7fffd5ac355c45c9e714
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-27 18:35:58 +0530

    9

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -7,30 +7,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun WaterCounter(modifier: Modifier = Modifier, count: Int, onCountChange: () -> Unit) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable {
-            mutableStateOf(0)
-        }
         if (count > 0) {
             Text(
-                text = "You have ${count} glasses",
+                text = "You have $count glasses",
             )
         }
-
         Button(
-            onClick = { count++ }, enabled = count < 10
+            onClick = onCountChange, enabled = count < 10
         ) {
             Text(text = "Add One")
         }
 
     }
 
+}
+
+@Composable
+fun StateFulWaterCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    WaterCounter(count = count, onCountChange = { count++ }, modifier = modifier)
 }
--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,9 +1,18 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    StateFulWaterCounter(Modifier.padding(8.dp))
 }
