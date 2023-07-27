d08b44ae7b91d252d39cd9e375ecb357c7a5e683
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-07-27 17:33:17 +0530

    done 7

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -14,18 +15,35 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember {
             mutableStateOf(0)
         }
         if (count > 0) {
+            var showTask by remember {
+                mutableStateOf(true)
+            }
+            if (showTask) {
+                WellnessTaskItem(taskName = "Have you taken 15 minutes to walk today?",
+                    onClose = { showTask = false })
+            }
             Text(
                 text = "You have ${count} glasses",
             )
         }
-        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp), enabled = count<10) {
-            Text(text = "Add One")
+        Row(modifier = Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ }, enabled = count < 10
+            ) {
+                Text(text = "Add One")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 16.dp),
+            ) {
+                Text(text = "Reset")
+            }
         }
     }
+
 }
---
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -0,0 +1,32 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    modifier: Modifier = Modifier,
+    taskName: String,
+    onClose: () -> Unit
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = taskName, modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+
+}
